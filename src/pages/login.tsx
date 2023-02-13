752a448500cbd6c4249a79d5da8f195cb91eb3a6
Lazizjon <ln41128n@pace.edu>
2023-02-13 18:39:51 -0500

    Added Google OAuth

--- src/pages/login.tsx
+++ src/pages/login.tsx
@@ -2,6 +2,8 @@ import { useEffect, useRef } from "react";
 import { useLogin } from "@pankod/refine-core";
 import { Container, Box } from "@pankod/refine-mui";
 
+import { yariga } from '../assets';
+
 import { CredentialResponse } from "../interfaces/google";
 
 export const Login: React.FC = () => {
@@ -18,7 +20,7 @@ export const Login: React.FC = () => {
       try {
         window.google.accounts.id.initialize({
           ux_mode: "popup",
-          client_id: "your-client-id",
+          client_id: process.env.REACT_APP_GOOGLE_CLIENT_ID,
           callback: async (res: CredentialResponse) => {
             if (res.credential) {
               login(res);
@@ -41,10 +43,7 @@ export const Login: React.FC = () => {
   return (
     <Box
       component="div"
-      sx={{
-        background: `radial-gradient(50% 50% at 50% 50%, #63386A 0%, #310438 100%)`,
-        backgroundSize: "cover",
-      }}
+      sx={{ backgroundColor: '#FCFCFC' }}
     >
       <Container
         component="main"
