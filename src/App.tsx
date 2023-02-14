cf3d1ea420ad9da3eea43ccd838091a162597686
Lazizjon <ln41128n@pace.edu>
2023-02-14 14:49:53 -0500

    Updated Issue With Getting TypeError Message

--- src/App.tsx
+++ src/App.tsx
@@ -60,7 +60,7 @@ function App() {
 
       // Save User To MongoDB
       if (profileObj) {
-        const response = await fetch('https://localhost:8080/api/v1/users', {
+        const response = await fetch('http://localhost:8080/api/v1/users', {
           method: 'POST',
           headers: { 'Content-Type': 'application/json' },
           body: JSON.stringify({
