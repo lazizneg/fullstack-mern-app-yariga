9919a6d5516cd77abde1e2a9edc4debe6d9b541b
Lazizjon <ln41128n@pace.edu>
2023-02-14 19:24:13 -0500

    Updated pages/all-properties.tsx

--- src/components/common/CustomButton.tsx
+++ src/components/common/CustomButton.tsx
@@ -5,6 +5,7 @@ import { CustomButtonProps } from 'interfaces/common';
 const CustomButton = ({ type, title, backgroundColor, color, fullWidth, icon, handleClick, disabled }: CustomButtonProps) => {
   return (
     <Button
+    disabled={disabled}
     type={type === 'submit' ? 'submit' : 'button'}
     sx={{
       flex: fullWidth ? 1 : 'unset',
--- src/pages/all-properties.tsx
+++ src/pages/all-properties.tsx
@@ -10,7 +10,13 @@ const AllProperties = () => {
   const navigate = useNavigate();
 
   const {
-    tableQueryResult: { data, isLoading, isError }
+    tableQueryResult: { data, isLoading, isError },
+    current,
+    setCurrent,
+    setPageSize,
+    pageCount,
+    sorter, setSorter,
+    filters, setFilters,
   } = useTable();
 
   const allProperties = data?.data ?? [];
@@ -20,8 +26,46 @@ const AllProperties = () => {
 
   return (
     <Box>
+      <Box mt="20px" sx={{ display: 'flex', flexWrap: 'wrap', gap: 3 }}>
+        <Stack direction="column" width="100%">
+          <Typography fontSize={25} fontWeight={700} color="#11142D">
+            {!allProperties.length ? 'There Are No Properties' : 'All Properties'}
+          </Typography>
+          <Box mb={2} mt={3} display="flex" width="84%" justifyContent="space-between" flexWrap="wrap">
+            <Box display="flex" gap={2} flexWrap="wrap" mb={{ xs: '20px', sm: 0 }}>
+              <CustomButton 
+                title={`Sort Price`}
+                handleClick={() => {}}
+                backgroundColor="#475BE8"
+                color="#FCFCFC"
+              />
+              <TextField 
+                variant="outlined"
+                color="info"
+                placeholder="Search By Title"
+                value=''
+                onChange={() => {}}
+              />
+              <Select
+                variant="outlined"
+                color="info"
+                displayEmpty
+                required
+                inputProps={{ 'aria-label': 'Without label' }}
+                defaultValue=""
+                value=''
+                onChange={() => {}}
+              >
+                <MenuItem value="">All</MenuItem>
+              </Select>
+            </Box>
+          </Box>
+        </Stack>
+      </Box>
+
+
+
       <Stack direction="row" justifyContent="space-between" alignContent="center">
-        <Typography fontSize={25} fontWeight={700} color="#11142D">All Properties</Typography>
         <CustomButton 
           title="Add Property"
           handleClick={() => navigate('/properties/create')}
@@ -43,8 +87,43 @@ const AllProperties = () => {
         />
         ))}
       </Box>
+
+      {allProperties.length > 0 && (
+        <Box display="flex" gap={2} mt={3} flexWrap="wrap">
+          <CustomButton 
+            title="Previous"
+            handleClick={() => setCurrent((prev) => prev - 1)}
+            backgroundColor="#475BE8"
+            color="#FCFCFC"
+            disabled={!(current > 1)}
+          />
+          <Box display={{ xs: 'hidden', sm: 'flex' }} alignItems="center" gap="5px">
+            Page{' '}<strong> {current} Of {pageCount} </strong>
+          </Box>
+          <CustomButton
+            title="Next"
+            handleClick={() => setCurrent((prev) => prev + 1)}
+            backgroundColor="#475BE8"
+            color="#FCFCFC"
+            disabled={current === pageCount}
+          />
+          <Select
+            variant="outlined"
+            color="info"
+            displayEmpty
+            required
+            inputProps={{ 'aria-label': 'Without label' }}
+            defaultValue={10}
+            onChange={() => {}}
+          >
+            {[10, 20, 30, 40, 50].map((size) => (
+              <MenuItem key={size} value={size}>Show {size}</MenuItem>
+            ))}
+          </Select>
+        </Box>
+      )}
     </Box>
   )
 }
 
-export default AllProperties
+export default AllProperties;
