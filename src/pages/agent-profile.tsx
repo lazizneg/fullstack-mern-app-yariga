d84b97360e796296c7496c78c47009fb1efb4637
Lazizjon <ln41128n@pace.edu>
2023-02-15 09:12:10 -0500

    Updated pages/agent-profile.tsx

--- src/pages/agent-profile.tsx
+++ src/pages/agent-profile.tsx
@@ -1,9 +1,35 @@
-import React from 'react'
+import { useOne } from '@pankod/refine-core';
+import { useParams } from '@pankod/refine-react-router-v6';
 
-const agentProfile = () => {
+import { Profile } from 'components';
+
+const AgentProfile = () => {
+  const { id } = useParams();
+
+  const { data, isLoading, isError } = useOne({
+    resource: 'users',
+    id: id as string,
+  });
+
+  const agentProfile = data?.data ?? [];
+
+  if (isLoading) {
+    return <div>Loading...</div>;
+  }
+
+  if (isError) {
+    return <div>Error...</div>;
+  }
+
   return (
-    <div>agent-profile</div>
+    <Profile 
+      type="Agent"
+      name={agentProfile?.name}
+      avatar={agentProfile?.avatar}
+      email={agentProfile?.email}
+      properties={agentProfile?.allProperties}
+    />
   )
 }
 
-export default agentProfile
+export default AgentProfile
