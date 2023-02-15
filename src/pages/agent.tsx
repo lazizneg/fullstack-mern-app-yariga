190c4ff732b56cb0ff2e818e35880d74167fa38f
Lazizjon <ln41128n@pace.edu>
2023-02-15 08:37:58 -0500

    Updated agent/AgentCard.tsx

--- src/pages/agent.tsx
+++ src/pages/agent.tsx
@@ -1,9 +1,48 @@
-import React from 'react'
+import { useList } from '@pankod/refine-core';
+import { Box, Typography } from '@pankod/refine-mui';
 
-const agent = () => {
+import { AgentCard } from 'components';
+
+const Agents = () => {
+  const { data, isLoading, isError } = useList({
+    resource: 'users',
+  });
+
+  const allAgents = data?.data ?? [];
+
+  if (isLoading) {
+    return <div>Loading...</div>;
+  }
+
+  if (isError) {
+    return <div>Error...</div>;
+  }
+
   return (
-    <div>agent</div>
-  )
-}
+    <Box>
+      <Typography fontSize={25} fontWeight={700} color="#11142D">Agent List</Typography>
 
-export default agent
+      <Box
+        mt="20px"
+        sx={{
+          display: 'flex',
+          flexWrap: 'wrap',
+          gap: '20px',
+          backgroundColor: '#FCFCFC' }}
+      >
+        {allAgents.length > 0 && allAgents?.map((agent) => (
+          <AgentCard
+            key={agent._id}
+            id={agent._id}
+            name={agent.name}
+            email={agent.email}
+            avatar={agent.avatar}
+            noOfProperties={agent.allProperties.length}
+          />
+        ))}
+      </Box>
+    </Box>
+  );
+};
+
+export default Agents;
